2b91c46d673c7e4081a3229e7f555eba6adccc05
Daniel Lucas <danomiterock@yahoo.com>
2021-03-11 10:22:58 -0700

    comments

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+//User selects favorite between joke and gif
+//create event listener for both the joke and gif
+//if neither is selected return to homepage
+// take input from selected button and place in an array in storage. Items in area are stored in class of joke or gif
+//create a function to recall and display favorite jokes and gifs based on which callback button is pressed. 
+//ensure array of favorites is displayed in a formatted container
+//call class and append to hidden favorites container
