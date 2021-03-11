949d7942ee797db1843c6abc09ba1f2f28d7d9b5
James V <james.vanhoke@yahoo.com>
2021-03-11 01:58:04 -0700

    Added Comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
+//Giphy API Key, delete after assignment
 var giphyAPIKey = "XRfxNr7yL9ahmuuB5NE74GlaO9Pem6su";
 
-//Reaches out to Giphy API with category
+//Reaches out to Giphy API
 function GiphyAPICall(e) {
   //Gets the data value of the button that was clicked
   var giphyCata = $(e.target).data("val");
-  //giphy API
+  //giphy API URL with our Key + query equal to the button the user clicks
   var giphyURL = `https://api.giphy.com/v1/gifs/search?api_key=${giphyAPIKey}&q=${giphyCata}`;
+  //calls the Giphy API
   fetch(giphyURL)
     .then((data) => data.json())
     .then(function (giphyData) {
@@ -18,23 +20,28 @@ function GiphyAPICall(e) {
     });
 }
 
+//Reaches out to the Joke API
 function JokeAPICall(e) {
+  //Grabs the data-joke from the clicked button
   var JokeCata = $(e.target).data("joke");
+  //JokeAPI URL, no APIKey needed, blacklist added to keep returns safe for work
   var JokeURL = `https://v2.jokeapi.dev/joke/${JokeCata}?blacklistFlags=nsfw,racist,sexist&type=single`;
-
+  //calls the JokeAPI
   fetch(JokeURL)
     .then((data) => data.json())
     .then(function (jokeData) {
-      console.log(jokeData);
       $("#testJokeHolder").text(jokeData.joke);
     });
 }
 
+//Adds Event listener to our category buttons, when a button is clicked invokes the GiphyAPI Function
 $(".testBtn").on(
   "click",
   GiphyAPICall
-); /*Update me with the permanent buttons class. Calls Giphy*/
+); /*Update me with the permanent buttons class.*/
+
+// Adds Event Listener to our category buttons, when a button is clicked invokes the JokeAPI Function.
 $(".testBtn").on(
   "click",
   JokeAPICall
-); /*Update me with the permanent buttons class. Calls JokeAPI*/
+); /*Update me with the permanent buttons class.*/
