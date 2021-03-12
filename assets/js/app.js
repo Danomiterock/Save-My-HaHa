bbfc26bd72a0058aced2685686c5027d33e97f36
James V <james.vanhoke@yahoo.com>
2021-03-12 12:53:32 -0700

    Fixed JS, Refactored code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,54 @@
+//Global Variables
+
+//Giphy API Key
 var giphyAPIKey = "XRfxNr7yL9ahmuuB5NE74GlaO9Pem6su";
-​
-//define storage
-/*
-  When the page loads
-  We want to look to localstorage and see if the user has any saved jokes or gifs
-*/
+
+var submitBtn = $(".testBtn");
+var gifHolder = $("#testGifHolder");
+var jokeHolder = $("testJokeHolder");
+
+//Storage variables
 var myStorage = window.localStorage;
 var storedGifs = myStorage.getItem("gifs");
 var storedJokes = myStorage.getItem("jokes");
-​
-/*
-  If our user does have saved information
-  We need to convert it from a JSON string
-  To a Javascript Data Structure
-  Note: In this case the data structure will be an array
-*/
+
+// Variables target the printed joke/gif
+var joke = $("#testJokeHolder");
+var giphy = $("#testGifHolder");
+
+//Initializes our Gif storage array
 if (storedGifs) {
+  // if our array exists in local storage. pull it
   storedGifs = JSON.parse(storedGifs);
 } else {
+  //if it doesn't exist, create a blank array
   storedGifs = [];
 }
-​
+
+//Initializes our Joke storage array
 if (storedJokes) {
+  // if our array exists in local storage. pull it
   storedJokes = JSON.parse(storedJokes);
 } else {
+  // if it doesn't exist, create a blank array
   storedJokes = [];
 }
-​
+
+//Event Listeners
+
+//Adds Event listener to our category buttons, when a button is clicked invokes the GiphyAPI Function
+submitBtn.on("click", GiphyAPICall);
+//Adds Event Listener to our category buttons, when a button is clicked invokes the JokeAPI Function.
+submitBtn.on("click", JokeAPICall);
+
+//Adds Event Listener on our Gif element
+giphy.on("click", saveGiphy);
+
+//Adds Event Listener on our Joke element
+joke.on("click", saveJoke);
+
+// Functions
+
 //Reaches out to Giphy API
 function GiphyAPICall(e) {
   //Gets the data value of the button that was clicked
@@ -35,23 +57,23 @@ function GiphyAPICall(e) {
   var giphyURL = `https://api.giphy.com/v1/gifs/search?rating=pg&api_key=${giphyAPIKey}&q=${giphyCata}`;
   //calls the Giphy API
   fetch(giphyURL)
-    .then((data) => data.json())
-    .then(function (giphyData) {
-      // Makes sure we get a random gif
-      var index = Math.floor(Math.random() * giphyData.data.length);
-      //variable for the random gifs hosted url
-      var gif = giphyData.data[index].images.original.url;
-      //sets our placeholder element to the gifs source, displaying it.
-      $("#testGifHolder").attr("src", gif);
-    });
+  .then((data) => data.json())
+  .then(function (giphyData) {
+    // Makes sure we get a random gif
+    var i = Math.floor(Math.random() * giphyData.data.length);
+    //variable for the random gifs hosted url
+    var gif = giphyData.data[i].images.original.url;
+    //sets our placeholder element to the gifs source, displaying it.
+    gifHolder.attr("src", gif);
+  });
 }
-​
+
 //Reaches out to the Joke API
 function JokeAPICall(e) {
   //Grabs the data-joke from the clicked button
   var JokeCata = $(e.target).data("joke");
   //JokeAPI URL, no APIKey needed, blacklist added to keep returns safe for work
-  var JokeURL = `https://v2.jokeapi.dev/joke/${JokeCata}?`;
+  var JokeURL = `https://v2.jokeapi.dev/joke/${JokeCata}?blacklist=nsfw,sexist,racist`;
   //calls the JokeAPI
   fetch(JokeURL)
     .then((data) => data.json())
@@ -59,51 +81,22 @@ function JokeAPICall(e) {
       printJoke(jokeData);
     });
 }
-​
+
 // Renders our recieved joke
 function printJoke(jokeData) {
   // Clears our previous Jokes
   $("#testJokeHolder").text("");
-​
 
-// If single-type joke is recieved from API
+  // If single-type joke is recieved from API
   if (jokeData.joke) {
     $("#testJokeHolder").text(jokeData.joke);
   } else {
     //else if double-type joke
     $("#testJokeHolder").html(jokeData.setup + "<br>" + jokeData.delivery);
   }
-  
-//Adds Event listener to our category buttons, when a button is clicked invokes the GiphyAPI Function
-$(".testBtn").on(
-  "click",
-  GiphyAPICall
-); /*Update me with the permanent buttons class.*/
-​
-// Adds Event Listener to our category buttons, when a button is clicked invokes the JokeAPI Function.
-$(".testBtn").on(
-  "click",
-  JokeAPICall
-); /*Update me with the permanent buttons class.*/
-​
-// Select an element
-// Vanilla -> document.querySelector()
-// jQuery -> $()
-​
-// Add event listener to the element
-// Vanilla -> addEventListener()
-// jQuery -> on()
-​
-//User selects favorite between joke and gif
-// Select the place holders on the page
-var joke = $("#testJokeHolder");
-var giphy = $("#testGifHolder");
-​
-//create event listener for both the joke and gif
-giphy.on("click", saveGiphy);
-​
-joke.on("click", saveJoke);
-​
+}
+
+
 //function to save gif
 function saveGiphy(e) {
   console.log(e);
@@ -118,7 +111,7 @@ function saveGiphy(e) {
   // Store this string in our localstorage
   myStorage.setItem("gifs", stringifiedData);
 }
-​
+
 //function to save Joke
 function saveJoke(e) {
   // myStorage.getitem(JokeAPICall);
@@ -132,16 +125,15 @@ function saveJoke(e) {
   var stringifiedData = JSON.stringify(storedJokes);
   // Store this string in our localstorage
   myStorage.setItem("joke", stringifiedData);
-  
 }
-​
+
 //if neither is selected return to homepage
 // take input from selected button and place in an array in storage. Items in area are stored in class of joke or gif
 //create a function to recall and display favorite jokes and gifs based on which callback button is pressed.
 //ensure array of favorites is displayed in a formatted container
 //call class and append to hidden favorites container
 //Giphy API Key, delete after assignment
-​
+
 //define items to be retrieved from storage
 // var giphValue = myStorage.getItem(GiphyAPICall);
 // var jokeValue = myStorage.getItem(JokeAPICall);
